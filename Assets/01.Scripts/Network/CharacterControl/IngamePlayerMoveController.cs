7497332f5c5857696d0d738077897a5824f64c4d
Songryan <tjr6255@gmail.com>
2024-05-05 22:45:21 +0900

    IngamePlayerMoveController 생성

---
+++ Assets/01.Scripts/Network/CharacterControl/IngamePlayerMoveController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
+
+public class IngamePlayerMoveController : NetworkBehaviour
+{
+    private Animator animator;
+    private CharacterController cc;
+
+    [SyncVar] private Vector2 moveVector = Vector2.zero;
+    private Vector2 moveVectorTarget;
+    private float moveSpeed = 2f;
+
+    private void Awake()
+    {
+        //GameObject tpsVCamInstance = Instantiate(tpsVCam);
+        //tpsVCam = tpsVCamInstance;
+        //CinemachineVirtualCamera fpsVCamInstance = Instantiate(fpsVCam).GetComponent<CinemachineVirtualCamera>();
+        //fpsVCam = fpsVCamInstance;
+        //Transform weaponPosInstance = Instantiate(weaponPos).GetComponent<Transform>();
+        //weaponPos = weaponPosInstance;
+        //Transform firePosInstance = Instantiate(firePos).GetComponent<Transform>();
+        //firePos = firePosInstance;
+        //Transform shellPosInstance = Instantiate(shellPos).GetComponent<Transform>();
+        //shellPos = shellPosInstance;
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
+
+        GameSystem.Instance.AddPlayer(this);
+    }
+
+    void Update()
+    {
+        MoveOrder();    // 이동  
+    }
+
+    private void MoveOrder()
+    {
+        animator.SetFloat("XSpeed", moveVector.x);
+        animator.SetFloat("ZSpeed", moveVector.y);
+
+        moveVector = Vector2.Lerp(moveVector, moveVectorTarget * moveSpeed, Time.deltaTime * 5);
+
+        Vector3 moveVector3 = new Vector3(moveVector.x * 0.5f, Physics.gravity.y, moveVector.y);
+
+        cc.Move(this.transform.rotation * moveVector3 * Time.deltaTime);
+    }
+
+    void OnMove(InputValue inputValue) // 이동(WASD)
+    {
+        Debug.Log($"isLocalPlayer : {isLocalPlayer}");
+        // Object가 Client 소유인지 여부 확인
+        if (isLocalPlayer)
+            moveVectorTarget = inputValue.Get<Vector2>(); // 인풋 벡터 받아옴
+    }
+
+    void OnSprint(InputValue inputValue)
+    {
+        // Object가 Client 소유인지 여부 확인
+        if (isLocalPlayer)
+        {
+            float value = inputValue.Get<float>();
+            moveSpeed = (value * 2f) + 2f;
+        }
+    }
+}
