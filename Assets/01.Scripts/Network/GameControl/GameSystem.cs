830df83facc6b84413f73ca448c41378df262e98
Songryan <tjr6255@gmail.com>
2024-05-05 22:45:21 +0900

    IngamePlayerMoveController 생성

---
+++ Assets/01.Scripts/Network/GameControl/GameSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameSystem : NetworkBehaviour
+{
+    public static GameSystem Instance;
+
+    public List<IngamePlayerMoveController> players = new List<IngamePlayerMoveController>();
+
+    public void AddPlayer(IngamePlayerMoveController player)
+    {
+        if (!players.Contains(player))
+        {
+            players.Add(player);
+        }
+    }
+
+    private IEnumerator GameReady()
+    {
+        var manager = NetworkManager.singleton as FPSRoomManager;
+        while(manager.roomSlots.Count != players.Count) 
+        {
+            yield return null;
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
